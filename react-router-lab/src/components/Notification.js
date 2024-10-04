098f11a38c357da24e69d67861af561c981f6b81
lopa9 <lopamudra.p188@gmail.com>
2024-10-04 10:13:59 +0000

    create and read

--- react-router-lab/src/components/Notification.js
+++ react-router-lab/src/components/Notification.js
@@ -1,44 +1,16 @@
 // src/components/Notification.js
 
-
-
 import React from 'react';
-import axios from 'axios';
 
-const notification = () => {
-  const output = axios.get("https://5000-lopa9-reactproject-uvf5uyxwoly.ws-us116.gitpod.io/persons");
-  console.log(output);
-  
-  const API_URL=process.env.REACT_APP_API_URL
-  console.log(API_URL);
+const Notification = ({ message, onClose }) => {
+  const notificationClass = `notification ${message.type}`;
 
- return (
-        <div>
-        <h2>this is the notification page</h2>
-        </div>
-      );
-    };
-    
-    export default notification;
+  return (
+    <div className={notificationClass}>
+      {message.text}
+      <button onClick={onClose} className="btn btn-cancel">Close</button>
+    </div>
+  );
+};
 
-
-
-
-
-
-
-// import React from 'react';
-
-// const Notification = ({ message, onClose }) => {
-//   const API_URL = process.env.REACT_APP_API_URL;
-//   const notificationClass = `notification ${message.type}`;
-
-//   return (
-//     <div className={notificationClass}>
-//       {message.text}
-//       <button onClick={onClose} className="btn btn-cancel">Close</button>
-//     </div>
-//   );
-// };
-
-// export default Notification;
+export default Notification;
--- react-router-lab/src/components/PersonList.js
+++ react-router-lab/src/components/PersonList.js
@@ -1,85 +1,63 @@
-// // src/components/PersonList.js
+// src/components/PersonList.js
 
-
-import React from 'react';
-//import personadd from './PersonAdd"
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
-//import notification from 
-const API_URL=process.env.REACT_APP_API_URL
-const output = axios.get("https://5000-lopa9-reactproject-uvf5uyxwoly.ws-us116.gitpod.io/persons");
+
+import Notification from './Notification';
+import '../styles/PersonList.css'; // Component-specific styles
+
+
+const API_URL = process.env.REACT_APP_API_URL;
+console.log(API_URL)
 
 
 const PersonList = () => {
-  console.log(output);
-  console.log(API_URL)
+  const [people, setPeople] = useState([]);
+  const [notification, setNotification] = useState('');
 
-    return(
-        <div>
-            <h2>to add list of  the person</h2>
-        </div>
-    )
 
-}
+  useEffect(() => {
+    const fetchPeople = async () => {
+      try {
+        const response = await axios.get(API_URL);
+        setPeople(response.data);
+      } catch (error) {
+        console.error('Error fetching people:', error);
+      }
+    };
+    fetchPeople();
+  }, []);
+
+  return (
+    <div className="person-list">
+      <h1>Person List</h1>
+      <Link to="/add" className="btn btn-add add-person-button">Add Person</Link>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+          </tr>
+        </thead>
+        <tbody>
+          {people.map(person => (
+            <tr key={person.id}>
+              <td>
+                <Link to={`/person/${person.id}`} className="person-name">
+                  {person.name}
+                </Link>
+              </td>
+              <td>{person.age}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {notification && (
+        <Notification message={notification} onClose={() => setNotification('')} />
+      )}
+    </div>
+  );
+};
+
 export default PersonList;
-
-
-
-
-// import React, { useEffect, useState } from 'react';
-// import { Link } from 'react-router-dom';
-// import axios from 'axios';
-
-// import Notification from './Notification';
-// import '../styles/PersonList.css'; // Component-specific styles
-
-// const API_URL = process.env.REACT_APP_API_URL;
-
-// const PersonList = () => {
-//   const [people, setPeople] = useState([]);
-//   const [notification, setNotification] = useState('');
-
-//   useEffect(() => {
-//     const fetchPeople = async () => {
-//       try {
-//         const response = await axios.get(API_URL);
-//         console.log(response);
-//         setPeople(response.data);
-//       } catch (error) {
-//         console.error('Error fetching people:', error);
-//       }
-//     };
-//     fetchPeople();
-//   }, []);
-
-//   return (
-//     <div className="person-list">
-//       <h1>Person List</h1>
-//       <Link to="/add" className="btn btn-add add-person-button">Add Person</Link>
-//       <table>
-//         <thead>
-//           <tr>
-//             <th>Name</th>
-//             <th>Age</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {people.map(person => (
-//             <tr key={person.id}>
-//               <td>
-//                 <Link to={`/person/${person.id}`} className="person-name">
-//                   {person.name}
-//                 </Link>
-//               </td>
-//               <td>{person.age}</td>
-//             </tr>
-//           ))}
-//         </tbody>
-//       </table>
-//       {notification && (
-//         <Notification message={notification} onClose={() => setNotification('')} />
-//       )}
-//     </div>
-//   );
-// };
-
-// export default PersonList;
