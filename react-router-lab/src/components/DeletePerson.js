26f8c7eda5e3f526dc7de2eae49e70785a7e013e
lopa9 <lopamudra.p188@gmail.com>
2024-10-04 04:03:00 +0000

    basic env connected

--- react-router-lab/src/components/DeletePerson.js
+++ react-router-lab/src/components/DeletePerson.js
@@ -1,20 +1,39 @@
+
 import React from 'react';
 
-const DeletePerson = ({ id, onDelete }) => {
-  
-  const API_URL = process.env.REACT_APP_API_URL;
+const handleDelete = () => {
+ 
+  const API_URL=process.env.REACT_APP_API_URL
 
-  const handleDelete = () => {
-    if (window.confirm('Are you sure you want to delete this person?')) {
-      onDelete(id);
-    }
-  };
+return (
+ <h1> this is the output</h1 >
 
-  return (
-    <button className="btn btn-delete" onClick={handleDelete}>
-      Delete
-    </button>
   );
 };
 
-export default DeletePerson;
+  export default DeletePerson;
+
+
+
+
+
+// import React from 'react';
+
+// const DeletePerson = ({ id, onDelete }) => {
+  
+//   const API_URL = process.env.REACT_APP_API_URL;
+
+//   const handleDelete = () => {
+//     if (window.confirm('Are you sure you want to delete this person?')) {
+//       onDelete(id);
+//     }
+//   };
+
+//   return (
+//     <button className="btn btn-delete" onClick={handleDelete}>
+//       Delete
+//     </button>
+//   );
+// };
+
+// export default DeletePerson;
--- react-router-lab/src/components/Notification.js
+++ react-router-lab/src/components/Notification.js
@@ -1,17 +1,44 @@
 // src/components/Notification.js
 
+
+
 import React from 'react';
+import axios from 'axios';
 
-const Notification = ({ message, onClose }) => {
-  const API_URL = process.env.REACT_APP_API_URL;
-  const notificationClass = `notification ${message.type}`;
+const notification = () => {
+  const output = axios.get("https://5000-lopa9-reactproject-7hmxmiw2yp8.ws-us116.gitpod.io/persons");
+  console.log(output);
+  
+  const API_URL=process.env.REACT_APP_API_URL
+  console.log(API_URL);
 
-  return (
-    <div className={notificationClass}>
-      {message.text}
-      <button onClick={onClose} className="btn btn-cancel">Close</button>
-    </div>
-  );
-};
+ return (
+        <div>
+        <h2>this is the notification page</h2>
+        </div>
+      );
+    };
+    
+    export default notification;
 
-export default Notification;
+
+
+
+
+
+
+// import React from 'react';
+
+// const Notification = ({ message, onClose }) => {
+//   const API_URL = process.env.REACT_APP_API_URL;
+//   const notificationClass = `notification ${message.type}`;
+
+//   return (
+//     <div className={notificationClass}>
+//       {message.text}
+//       <button onClick={onClose} className="btn btn-cancel">Close</button>
+//     </div>
+//   );
+// };
+
+// export default Notification;
